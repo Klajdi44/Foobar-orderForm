0d1b9728cb900bb3c6db6ba6f343bb6b991dcacd
Gintare <gintaar3@gmail.com>
2020-12-06 02:40:16 +0100

    overall order

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -21,6 +21,31 @@ function Menu(props) {
 
       <div className="order">
         <h1>Your Order</h1>
+        <div className="order-flex">
+          <div className="order-cards">
+            <ul>
+              <li>
+                <h1>El Hefe</h1>
+                <br />
+                <div className="menu-order_order">
+                  <h3>50DKK</h3>
+                  <button>-</button>
+                  <span>1</span>
+                  <button>+</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="order-price">
+            <div className="overall-price">
+              <h1>Total</h1>
+              <span>50DKK</span>
+              <br />
+            </div>
+
+            <button>Proceed To Checkout</button>
+          </div>
+        </div>
       </div>
     </article>
   );
