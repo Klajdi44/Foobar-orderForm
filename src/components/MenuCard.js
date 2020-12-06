992e4fd705a63ebc9ab37ee7fdbbd9f4cffd8eb4
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2020-12-06 14:07:54 +0100

    getting images

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -1,19 +1,17 @@
 
 function MenuCard(props) {
-
 	const beersFromList = props.apiData[0];
 	const beersFromTap = props.apiData[1].taps.map((beer) => beer.beer);
+	const filteredBeers = beersFromList.filter((beers) => beersFromTap.includes(beers.name));
 
-	const filteredBeers = beersFromList.filter((beers) =>
-		beersFromTap.includes(beers.name)
-	);
 
 	return (
 		filteredBeers.map(beer => {
 			return (
+
 				<div className="menu-card">
 					<div className="menu-content">
-						<img src="https://www.bottleyourbrand.com/media/dol/design/1/14886272141133341347158559.png" alt="Beer"></img>
+						<img src={`/images/${beer.label}`} alt="Beer"></img>
 						<h1>{beer.name}</h1>
 						<p>{beer.description.overallImpression}</p>
 					</div>
