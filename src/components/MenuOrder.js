e7fb926110ffd8db0e8e7391f4d67fb02b9625b1
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2020-12-06 15:05:52 +0100

    commented price

--- src/components/MenuOrder.js
+++ src/components/MenuOrder.js
@@ -2,23 +2,23 @@ import React, { useState } from 'react';
 
 function MenuOrder(props) {
 	const [beerCount, setBeerCount] = useState(0);
-	const [price, setPrice] = useState(50);
+	// const [price, setPrice] = useState(50);
 
 	function addBeer() {
 		setBeerCount(beerCount + 1);
-		setPrice(price + 50);
+		// setPrice(price + 50);
 	}
 
 	function removeBeer() {
 		beerCount === 0 ? setBeerCount(0) : setBeerCount(beerCount - 1);
 
-		price === 50 ? setPrice(50) : setPrice(price - 50);
+		// price === 50 ? setPrice(50) : setPrice(price - 50);
 	}
 
 
 	return (
 		<div className="menu-order">
-			<h3>{price}DKK</h3>
+			<h3>50DKK</h3>
 			<button onClick={removeBeer}>-</button>
 			<span>{beerCount}</span>
 			<button onClick={addBeer} >+</button>
