0d8e2b6080008c225feef2c8fafe92926ab0ea43
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2020-12-09 19:46:25 +0100

    resetting orders done

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,10 +1,22 @@
+import React, { useContext } from 'react'
+import { OrderContext } from './OrderContext';
+
 function Confirmation(props) {
+
+  const [orderObj, setOrderObj] = useContext(OrderContext);
+
+  const resetBeers = orderObj.map(({ amount, price, ...restOfData }) => ({ ...restOfData, amount: 0, price: 0 }));
+
+
   return (
     <section className="confirmation-wrapper">
       <h1>{props.confirmationP}</h1>
       <h2>hello</h2>
-      <button onClick={() => props.setPage('orderPage')}>Order again</button>
-    </section>
+      <button onClick={() => {
+        props.setPage('orderPage');
+        setOrderObj(resetBeers);
+      }}>Order again</button>
+    </section >
   );
 }
 
