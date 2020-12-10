0f161e27b597ebda89bf48374cdc9dc9d8e0a952
klajdi44 <klajdi1996@yahoo.gr>
2020-12-10 17:30:15 +0100

    confirmation page done

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { OrderContext } from './OrderContext';
+import Checkmark from './Checkmark';
 
 function Confirmation(props) {
 
@@ -11,6 +12,7 @@ function Confirmation(props) {
   return (
     <section className="confirmation-wrapper">
       <h1>Thank you for your order</h1>
+      <Checkmark />
       <h2>Your order number is <span> #{props.confirmationP}</span></h2>
       <button onClick={() => {
         props.setPage('orderPage');
