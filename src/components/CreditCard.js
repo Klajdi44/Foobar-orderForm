22a075b12b6f6558ba43fbf5fc84d2cd6dfc090b
Gintare <gintaar3@gmail.com>
2020-12-10 08:28:11 +0100

    some form styling

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,145 +1,124 @@
 import React from "react";
 import Card from "react-credit-cards";
 
-import {
-	formatCreditCardNumber,
-	formatCVC,
-	formatExpirationDate,
-	formatFormData,
-	allLetter
-} from "../modules/utils";
+import { formatCreditCardNumber, formatCVC, formatExpirationDate, formatFormData, allLetter } from "../modules/utils";
 
 import "react-credit-cards/es/styles-compiled.css";
 
 export default class CreditCard extends React.Component {
-	state = {
-		number: "",
-		name: "",
-		expiry: "",
-		cvc: "",
-		issuer: "",
-		focused: "",
-		formData: null
-	};
+  state = {
+    number: "",
+    name: "",
+    expiry: "",
+    cvc: "",
+    issuer: "",
+    focused: "",
+    formData: null,
+  };
 
-	handleCallback = ({ issuer }, isValid) => {
-		if (isValid) {
-			this.setState({ issuer });
-		}
-	};
+  handleCallback = ({ issuer }, isValid) => {
+    if (isValid) {
+      this.setState({ issuer });
+    }
+  };
 
-	handleInputFocus = ({ target }) => {
-		this.setState({
-			focused: target.name
-		});
-	};
+  handleInputFocus = ({ target }) => {
+    this.setState({
+      focused: target.name,
+    });
+  };
 
-	handleInputChange = ({ target }) => {
-		if (target.name === "number") {
-			target.value = formatCreditCardNumber(target.value);
-		} else if (target.name === "expiry") {
-			target.value = formatExpirationDate(target.value);
-		} else if (target.name === "cvc") {
-			target.value = formatCVC(target.value);
-		}
+  handleInputChange = ({ target }) => {
+    if (target.name === "number") {
+      target.value = formatCreditCardNumber(target.value);
+    } else if (target.name === "expiry") {
+      target.value = formatExpirationDate(target.value);
+    } else if (target.name === "cvc") {
+      target.value = formatCVC(target.value);
+    }
 
-		this.setState({ [target.name]: target.value });
-	};
+    this.setState({ [target.name]: target.value });
+  };
 
-	handleSubmit = (e) => {
-		// e.preventDefault();
-		const formData = [...e.target.elements]
-			.filter((d) => d.name)
-			.reduce((acc, d) => {
-				acc[d.name] = d.value;
-				return acc;
-			}, {});
+  handleSubmit = (e) => {
+    // e.preventDefault();
+    const formData = [...e.target.elements]
+      .filter((d) => d.name)
+      .reduce((acc, d) => {
+        acc[d.name] = d.value;
+        return acc;
+      }, {});
 
-		this.setState({ formData });
-		this.form.reset();
-	};
+    this.setState({ formData });
+    this.form.reset();
+  };
 
+  render() {
+    const { name, number, expiry, cvc, focused, issuer, formData } = this.state;
 
-
-	render() {
-		const { name, number, expiry, cvc, focused, issuer, formData } = this.state;
-
-		return (
-			<div key="Payment">
-				<div className="App-payment">
-					<Card
-						number={number}
-						name={name}
-						expiry={expiry}
-						cvc={cvc}
-						focused={focused}
-						callback={this.handleCallback}
-					/>
-					<div ref={(c) => (this.form = c)} onSubmit={this.handleSubmit}>
-						<div className="form-group">
-							<input
-								type="tel"
-								name="number"
-								className="form-control"
-								placeholder="Card Number"
-								pattern="[\d| ]{16,22}"
-								required
-								onChange={this.handleInputChange}
-								onFocus={this.handleInputFocus}
-							/>
-							<small>E.g.: 49..., 51..., 36..., 37...</small>
-						</div>
-						<div className="form-group">
-							<input
-								type="text"
-								name="name"
-								className="form-control"
-								placeholder="Name"
-								required
-								onInput={(e) => allLetter(e.target)}
-								onChange={this.handleInputChange}
-								onFocus={this.handleInputFocus}
-							/>
-						</div>
-						<div className="row">
-							<div className="col-6">
-								<input
-									type="tel"
-									name="expiry"
-									className="form-control"
-									placeholder="Valid Thru"
-									pattern="(0[1-9]|10|11|12)/2[0-9]{3}$"
-									required
-									onChange={this.handleInputChange}
-									onFocus={this.handleInputFocus}
-								/>
-							</div>
-							<div className="col-6">
-								<input
-									type="tel"
-									name="cvc"
-									className="form-control"
-									placeholder="CVC"
-									pattern="\d{3,4}"
-									required
-									onChange={this.handleInputChange}
-									onFocus={this.handleInputFocus}
-								/>
-							</div>
-						</div>
-						<input type="hidden" name="issuer" value={issuer} />
-						<div className="form-actions">
-						</div>
-					</div>
-					{formData && (
-						<div className="App-highlight">
-							{formatFormData(formData).map((d, i) => (
-								<div key={i}>{d}</div>
-							))}
-						</div>
-					)}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div key="Payment">
+        <div className="App-payment">
+          <Card number={number} name={name} expiry={expiry} cvc={cvc} focused={focused} callback={this.handleCallback} />
+          <div className="form" ref={(c) => (this.form = c)} onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label for="card-number">Credit Card</label>
+              <span className="form-span">Enter your card number</span>
+              <input
+                type="tel"
+                name="number"
+                className="form-control"
+                placeholder="**** ***** **** *****"
+                pattern="[\d| ]{16,22}"
+                required
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </div>
+            <div className="form-group">
+              <label for="name">Cardholder name</label>
+              <span className="form-span">Enter first and last name on the card</span>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                placeholder="e.g. John Smith"
+                required
+                onInput={(e) => allLetter(e.target)}
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </div>
+            <div className="form-group exp">
+              <label for="exp">Exp Date</label>
+              <span class="form-span">e.g. 07/2020</span>
+              <input
+                type="tel"
+                name="expiry"
+                className="form-control"
+                placeholder="MM/YYYY"
+                pattern="(0[1-9]|10|11|12)/2[0-9]{3}$"
+                required
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </div>
+            <div className="form-group cvc">
+              <span class="form-span">CVC</span>
+              <input type="tel" name="cvc" className="form-control" placeholder="***" pattern="\d{3,4}" required onChange={this.handleInputChange} onFocus={this.handleInputFocus} />
+            </div>
+            <input type="hidden" name="issuer" value={issuer} />
+            <div className="form-actions"></div>
+          </div>
+          {formData && (
+            <div className="App-highlight">
+              {formatFormData(formData).map((d, i) => (
+                <div key={i}>{d}</div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,60 +1,65 @@
-import React, { useContext } from 'react';
-import CreditCard from './CreditCard';
-import { OrderContext } from './OrderContext';
-import postData from '../modules/postData';
+import React, { useContext } from "react";
+import CreditCard from "./CreditCard";
+import { OrderContext } from "./OrderContext";
+import postData from "../modules/postData";
 
 function Form(props) {
   const [orderObj] = useContext(OrderContext);
-  let filteredPostOrders =
-    orderObj.map((order) => {
-      return order.amount < 1 ? undefined : { name: order.name, amount: Number(order.amount) }
-    });
+  let filteredPostOrders = orderObj.map((order) => {
+    return order.amount < 1 ? undefined : { name: order.name, amount: Number(order.amount) };
+  });
 
-  filteredPostOrders = filteredPostOrders.filter(order => {
+  filteredPostOrders = filteredPostOrders.filter((order) => {
     return order !== undefined;
   });
 
-
   function orderBeers() {
-
-    postData(filteredPostOrders, 'https://foobar-exam-data.herokuapp.com/order', (data) => {
-      return data.message === "added" ? <div>
-        {props.setPage('confirmationPage')}
-        {props.setConfirmationP(data.id)}
-        {console.log(data.id)}
-
-      </div> : null;
-    })
+    postData(filteredPostOrders, "https://foobar-exam-data.herokuapp.com/order", (data) => {
+      return data.message === "added" ? (
+        <div>
+          {props.setPage("confirmationPage")}
+          {props.setConfirmationP(data.id)}
+          {console.log(data.id)}
+        </div>
+      ) : null;
+    });
   }
-
 
   return (
     <article className="form-wrapper">
-      <div>
-        <h1>I am a beautiful form</h1>
-        <button onClick={() => props.setPage('orderPage')}> Go Back</button>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          orderBeers();
-        }}>
+      <div className="cc-wrapper">
+        {/* <button onClick={() => props.setPage("orderPage")}> Go Back</button> */}
+        <h1>Payment</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            orderBeers();
+          }}
+        >
           <CreditCard />
-          <button >Submit</button>
+          <button className="form-btn">Order</button>
         </form>
       </div>
-      <div>
-        {orderObj.map((order) => {
-          const imageUrl = order.name.replace(/\s+/g, "").toLowerCase();
-          if (order.amount > 0) {
-            return (
-              <li>
-                <img src={`images/${imageUrl}.png`} alt="Order Beer"></img>
-                <h1>{order.name}</h1>
-                <span>{order.amount}</span>
-              </li>
-            );
-          }
-          return null;
-        })}
+      <div className="order-list-wrapper">
+        <h1>Your Order</h1>
+        <ul>
+          {orderObj.map((order) => {
+            const imageUrl = order.name.replace(/\s+/g, "").toLowerCase();
+            if (order.amount > 0) {
+              return (
+                <li>
+                  <img src={`images/${imageUrl}.png`} alt="Order Beer"></img>
+                  <div>
+                    <h2>{order.name}</h2>
+                    <span>Quantity {order.amount}</span>
+                  </div>
+                  <span>{order.price}DKK</span>
+                </li>
+              );
+            }
+            return null;
+          })}
+        </ul>
       </div>
     </article>
   );
