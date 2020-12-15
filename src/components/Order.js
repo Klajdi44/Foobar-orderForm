e38a766584bf0d8c278ef091aa0f3866bb4f96ef
klajdi44 <klajdi1996@yahoo.gr>
2020-12-15 12:52:28 +0100

    virst version of filter

--- src/components/Order.js
+++ src/components/Order.js
@@ -8,11 +8,16 @@ import { OrderContext } from "./OrderContext";
 function Order(props) {
   const [page, setPage] = useState("orderPage");
   const [confirmationP, setConfirmationP] = useState("");
+  const [category, setCategory] = useState('All');
 
   const beersFromList = props.apiData[0];
   const beersFromTap = props.apiData[1].taps.map((beer) => beer.beer);
   const filteredBeers = beersFromList.filter((beers) => beersFromTap.includes(beers.name));
   const unavailableBeers = beersFromList.filter((beers) => !beersFromTap.includes(beers.name));
+  let updated = filteredBeers.filter(beer => beer.category === category);
+  if (category === 'All') {
+    updated = filteredBeers
+  }
 
   // eslint-disable-next-line
   const [orderObj, setOrderObj] = useContext(OrderContext);
@@ -24,10 +29,21 @@ function Order(props) {
     setOrderObj(orders);
     // eslint-disable-next-line
   }, []);
+  console.log(category);
 
+  function updateCategory(e) {
+    setCategory(e.target.innerText);
+  }
   return (
     <section>
-      {page === "orderPage" ? <Menu setPage={setPage} apiData={props.apiData} filteredBeers={filteredBeers} unavailableBeers={unavailableBeers} /> : null}
+      <div>
+        <button disabled={category === 'All'} onClick={updateCategory}>All</button>
+        {filteredBeers.map(beer => {
+          return (
+            <button disabled={beer.category === category} onClick={updateCategory}>{beer.category}</button>
+          )
+        })} </div>
+      {page === "orderPage" ? <Menu setPage={setPage} apiData={props.apiData} filteredBeers={updated} unavailableBeers={unavailableBeers} /> : null}
       {page === "formPage" ? <Form setPage={setPage} setConfirmationP={setConfirmationP} /> : null}
       {page === "confirmationPage" ? <Confirmation confirmationP={confirmationP} setPage={setPage} /> : null}
     </section>
